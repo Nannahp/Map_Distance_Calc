a99d6a3264684555b5a641842cfd49c5c136b310
Nannahp <143876673+Nannahp@users.noreply.github.com>
2025-03-19 09:58:54 +0100

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://gyzrifujxztrhypzbxam.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd5enJpZnVqeHp0cmh5cHpieGFtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDIwMzE4MjIsImV4cCI6MjA1NzYwNzgyMn0.2snJvIs9RCYodysb78eTkooYo5kHJsMCncNSAaxaIcM';
-
+const SUPABASE_URL = 'url';
+const SUPABASE_ANON_KEY = 'api'
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
